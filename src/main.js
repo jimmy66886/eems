a9816d776970828a6220780958ac21481fb02e41
zzmr <jim7yang@gmail.com>
2023-12-10 19:14:45 +0800

    初始化

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import 'vue-beauty/package/style/vue-beauty.min.css'
+import vueBeauty from 'vue-beauty'
+import axios from './js/axios'
+Vue.use(vueBeauty)
+Vue.config.productionTip = false
+
+// 按需引入
+// 引入ElementUI组件库
+import ElementUI from 'element-ui';
+// 引入ElementUI的全部样式
+import 'element-ui/lib/theme-chalk/index.css';
+
+// 应用ElementUI
+Vue.use(ElementUI)
+
+new Vue({
+  router,
+  render: h => h(App),
+  beforeCreate() {
+    Vue.prototype.$bus = this //安装全局事件总线
+  }
+}).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+import VueRouter from "vue-router";
+
+// 创建并暴露一个路由器
+export default new VueRouter({
+    routes: [
+        // {
+        //     path: '/home',
+        //     component: Home
+        // }
+    ],
+})
